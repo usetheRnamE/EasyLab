7ec530a9aa033e4c66667736a2c5b326bf5c2f01
reAuthorName <liubomyrcherevatyy@gmail.com>
2022-09-30 09:49:53 +0300

    Random error calculation

--- EasyLab/EasyLab/RandomError.cpp
+++ EasyLab/EasyLab/RandomError.cpp
@@ -1,10 +1,52 @@
 #include "RandomError.h"
+#include "Data.h"
 
-double RandomError::AvarageVal(const std::vector<double>& data)
+void RandomError::CalculateRandErr()
+{
+	if(!RandErrExist()) 
+	{
+	   //return to main and say that random error doesn`t exist
+		return;
+	}
+
+
+}
+
+bool RandomError::RandErrExist()
+{
+	if(data->GetData().size() == 1) return true;
+
+    avarage = AvarageVal();
+
+	for(auto& value : data->GetData())
+	   if(avarage ==  value) return true;
+
+	return false;
+}
+
+double RandomError::SumCount(double (*function)(const double&))
 {
 	double sum = 0;
-	for (auto& value : data)
-		sum += value;
 
-	return sum / data.size();
+	for (auto& value : data->GetData())
+		sum += function(value);
+
+	sum /= data->GetData().size();
+
+	return sum;
+}
+
+double RandomError::AvarageVal()
+{
+	return SumCount([](const double& val) { return val; });
+}
+
+double RandomError::FindDeltasSum()
+{
+	return SumCount([](const double& val) { return pow(val - avarage, 2); });
+}
+
+double RandomError::Func()
+{
+	
 }
