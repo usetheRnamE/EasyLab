d1e3f4685125da893f6bde8b7f45543615aae663
reAuthorName <liubomyrcherevatyy@gmail.com>
2022-10-05 19:56:20 +0300

    SOLID implementation

--- EasyLab/EasyLab/GrossError.h
+++ EasyLab/EasyLab/GrossError.h
@@ -2,23 +2,30 @@
 #include <vector>
 #include <iostream>
 
+#include "ErrorsInterface.h"
+
 struct ExtremumID
 {
 	unsigned int max = -1, min = -1;
 };
 
-class GrossError
+class GrossError : public ErrorsInterface
 {
 private:
 	 ExtremumID* Id;
+	 Data* data;
 
 public:
-	void GrossErrorDetection();
-
-	GrossError() { ExtremumID* Id = new ExtremumID(); }
+	GrossError() : Id(new ExtremumID()), data(Data::GetInstance()) {}
 	~GrossError() { delete Id; }
-	
+
+	void ErrorDetection() override
+	{
+		GetGrossError(data->GetData());
+	}
+
 private:
+	
 	void GetGrossError(const std::vector<double>& data);
 	void AskConfirmation();
 	void DeleteGrossError(const bool deleteMin, const bool deleteMax);
